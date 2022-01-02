0f577bd34021cee48d380cb01af532b1f73e8025
tuguldurs <tuguldur.s@gmail.com>
2022-01-02 00:36:06 -0500

    init setup

---
+++ plais/__init__.py
@@ -0,0 +1,9 @@
+import os
+from pathlib import Path
+
+# package directories
+package_path = Path(os.path.dirname(__file__)).parent
+package_data_path = package_path / 'data'
+
+# package imports
+from .plais import Plais
