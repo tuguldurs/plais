b7f71c6e6145ada662decd89fc27b910ac2dea6b
Tuguldur Sukhbold <tuguldur.s@gmail.com>
2022-01-17 17:35:07 -0500

    residual signal

--- plais/__main__.py
+++ plais/__main__.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 import logging
 
+from tqdm import tqdm
+
 from .recording import Recording
 from .frame import Frame, MedianVoxel
 
@@ -14,12 +16,20 @@ __all__ = ['plais']
 
 
 def main(args) -> None:
-    # args.start
-    # args.end
     # args.speed
     rec = Recording(args.FileChooser)
-    frame = Frame(rec.frame(0))
-    mask = MedianVoxel(rec, 0, 180).filter
+    median_filter = MedianVoxel(rec, 0, 180).filter
+
+    log.info(f'analysis duration {args.start} - {args.end} [sec]')
+    for i in tqdm(range(args.start, args.end)):
+        idx = i * rec.fps
+        idx_next = (i + 1) * rec.fps
+        frame = Frame(rec.frame(idx)).crop
+        frame_next = Frame(rec.frame(idx_next)).crop
+        absdiff = abs(frame_next - frame)
+        residual = median_filter - absdiff
+        residual[residual > 0] = 0
+        residual = abs(residual)
 
 
 if __name__ == '__main__':
