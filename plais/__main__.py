ec8e59c97d0804f0541addfa21b02731e67cc77e
Tuguldur Sukhbold <tuguldur.s@gmail.com>
2022-01-16 23:24:22 -0500

    logging setup

--- plais/__main__.py
+++ plais/__main__.py
@@ -1,9 +1,13 @@
 from __future__ import annotations
 
-import logging.config
+import logging
 
 from .recording import Recording
 
+log  = logging.getLogger(__name__)
+fmt = '%(asctime)s ~ %(name)14s ~ %(levelname)8s ::: %(message)s'
+lvl = logging.INFO
+logging.basicConfig(level=lvl, format=fmt)
 
 __all__ = ['plais']
 
