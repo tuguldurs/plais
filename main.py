b093f385418a5c3b84fbb8fe5c1c2b7269a99a4c
Tuguldur Sukhbold <tuguldur.s@gmail.com>
2022-01-24 22:45:26 -0500

    bounding box for detection

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from __future__ import annotations
 
 import logging.config
 
+import numpy as np
 from tqdm import tqdm
-from numpy import arange as nparange
 from multiprocessing import Pool, cpu_count, freeze_support
 from gooey import Gooey, GooeyParser
 from imageio import get_reader
@@ -62,12 +62,11 @@ class Args:
 
 def tst():
     fname = 'd/steel_line_test.mp4'
-    args = Args(fname, 1150, 1160, 40, 10)
+    args = Args(fname, 1150, 1200, 40, 10)
     p = Plais(args)
     p.run()
 
 ##########################################################################################
-
 
 class Plais:
     """Prod-Line ai system for Wolverine."""
@@ -91,40 +90,57 @@ class Plais:
 
         TODO: this is a terrible parallelization, need shared memory for rec object.
         """
-        rec = Recording(self.fname)
         with Pool(self.n_cpu) as pool:
             log.info(f'collecting frames for filter with {self.n_cpu} workers...')
             results = [pool.apply_async(self._process_frame, (idx * rec.fps, self.fname)) for idx in range(100)]
             frames = [result.get() for result in tqdm(results)]
         return frames
 
+    @staticmethod
+    def _bounding_box(img, pad=50):
+        """Bounding box of binary image."""
+        y = np.any(img, axis=1)
+        x = np.any(img, axis=0)
+        xmin, xmax = np.where(x)[0][[0, -1]]
+        ymin, ymax = np.where(y)[0][[0, -1]]
+        return xmin-pad, xmax+pad, ymin-pad, ymax+pad
+
     def run(self):
+        """Driver."""
         rec = Recording(self.fname)
+
+        # end time
         if not self.tend:
             self.tend = int(rec.duration) - 2
+
+        # median filter
         frames = self._collect_frames(rec)
         median_filter = MedianVoxel(frames).filter
         log.info('filter created.')
 
+        # indices by second
         sec_multiplier = 2
         log.info(f'analysis duration {self.tstart} - {self.tend} [sec]')
         log.info(f'frame fetch rate - every {sec_multiplier} second')
         n_steps = (self.tend - self.tstart) // sec_multiplier
-        idxs = nparange(n_steps) * sec_multiplier + self.tstart
+        idxs = np.arange(n_steps) * sec_multiplier + self.tstart
 
-        for i, idx in tqdm(enumerate(idxs)):
-            if i == len(idxs) - 1:
-                break
-            frame = rec.frame(idx)
-            frame_next = rec.frame(idxs[i+1])
+        record = []
+        for i, idx in tqdm(enumerate(idxs[:-1])):
+            frame = rec.frame(idx * rec.fps)
+            frame_next = rec.frame(idxs[i+1] * rec.fps)
             residual = Residual(frame, frame_next, median_filter, self.sensitivity)
 
             if residual.signal > 1000:
                 issue = True
+                bbox = self._bounding_box(residual.map)
             else:
                 issue = False
+                bbox = ()
 
-            print(idx, issue, residual.signal)
+            record.append((idx, issue, bbox, residual.signal))
+
+            print(idx, bbox, issue, residual.signal)
 
 
 if __name__ == '__main__':
